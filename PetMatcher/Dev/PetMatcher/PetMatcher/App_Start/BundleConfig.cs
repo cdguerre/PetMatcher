b8af776baf3647f937dfbf3ccbd0a210c0d7f8b0
Chaz Guerrero <cdguerre@gmail.com>
2014-10-18 16:53:27 -0700

    added angular and moment

--- PetMatcher/Dev/PetMatcher/PetMatcher/App_Start/BundleConfig.cs
+++ PetMatcher/Dev/PetMatcher/PetMatcher/App_Start/BundleConfig.cs
@@ -18,6 +18,10 @@ namespace PetMatcher
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-route.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
